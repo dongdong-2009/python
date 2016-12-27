1c05c080030a568a9bd3dce5ec2723d57d40efe4
小昆哥 <969352531@qq.com>
2016-12-27 08:15:11 +0800

    增加网易第三课语法算法练习
    Signed-off-by: 小昆哥 <969352531@qq.com>

---
+++ base/grammar/connect.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+if(__name__=="__main__"):
+	display = "I\
+			   am\
+			   mkk"
+	print(display)
---
+++ base/grammar/quotes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+str1 = 'hello world 1'
+str2 = "hello world 2"
+str3 = '''hello world 3'''
+#三引号支持多行
+str4 = """hello 
+world 4"""
+
+
+print(str1+"\n"+str2+"\n"+str3+"\n"+str4)
