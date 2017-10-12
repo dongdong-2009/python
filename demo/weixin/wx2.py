f2ed8e3cd4371b695a64bf019a45d1d738cc4718
小昆哥 <969352531@qq.com>
2017-10-12 23:45:09 +0800

    aa

---
+++ demo/weixin/wx2.py
@@ -0,0 +1,14 @@
+import itchat
+
+
+# 注册消息响应事件，消息类型为itchat.content.TEXT，即文本消息
+@itchat.msg_register(itchat.content.TEXT)
+def text_reply(msg):
+    # 返回同样的文本消息
+    #return msg['Text']
+    return "aaaaa"
+
+
+itchat.auto_login()
+# 绑定消息响应事件后，让itchat运行起来，监听消息
+itchat.run()
